331a8ed3db63d42216e82570cb705ee35f1b9871
David Dengg <david.dengg@gmail.com>
2021-03-18 18:44:45 +0100

    Updates AnylineJS to 29

--- Demo/html/index.js
+++ Demo/html/index.js
@@ -1,121 +1,54 @@
-const { init, errorCodes } = window.anylinejs;
-
-const entityMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-  '/': '&#x2F;',
-  '`': '&#x60;',
-  '=': '&#x3D;',
-};
-
-function escapeHtml(string) {
-  return String(string).replace(/[&<>"'`=\/]/g, function(s) {
-    return entityMap[s];
-  });
-}
-
-function replaceVerticalBar(string) {
-  return String(string).replace(/[|]/g, function(s) {
-    return '\n';
-  });
-}
-
 const viewConfig = {
-  // captureResolution: '1080p',
   outerColor: '000000',
   outerAlpha: 0.5,
   cutouts: [
     {
+      cancelOnResult: false,
       cutoutConfig: {
-        // style: 'rect',
-        maxWidthPercent: '80%',
-        alignment: 'top_half',
-        ratioFromSize: {
-          width: 300,
-          height: 250,
-        },
-        width: 720,
         strokeWidth: 2,
         cornerRadius: 4,
         strokeColor: 'FFFFFFFF',
         feedbackStrokeColor: '0099FF',
       },
-      // "delayStartScanTime": 2000,
       scanFeedback: {
-        style: 'rect',
+        style: 'contour_point',
         strokeColor: '0099FF',
         fillColor: '300099FF',
         strokeWidth: 2,
         cornerRadius: 4,
-        animationDuration: 150,
-        animation: 'NONE',
-        // beepOnResult: true,
-        // vibrateOnResult: true,
-        // blinkAnimationOnResult: true,
       },
     },
   ],
 };
 
-
 const root = document.getElementById('root');
 let selectedPreset = undefined;
+let Anyline;
 
 function mountAnylineJS(preset) {
+  try{
+
   selectedPreset = preset;
-  const Anyline = init({
-    config: {},
+
+  Anyline = window.anylinejs.init({
+    config: {
+      estimateMainCamera: !new URLSearchParams(window.location.search).get('c'),
+    },
     preset: preset.value,
     viewConfig,
     license: demoLicense,
     element: root,
     debugAnyline: true,
-    anylinePath: '../../anylinejs/anylinejs'
+    anylinePath: '../anylinejs/anylinejs'
   });
-
-  let modalOpen = false;
 
   Anyline.onResult = result => {
     console.log('Result: ', result);
-    if (modalOpen) return;
-    // Anyline.stopScanning();
-    window.Swal.fire({
-      title: 'Result',
-      html: `<div class="result">${result.result
-        .map(
-          resultEl =>
-            `
-        <div class="resultRow">
-          <div class="resultLabel">${resultEl.identifier}:</div>
-          <div>${escapeHtml(replaceVerticalBar(resultEl.text)) || 'kA'}</div>
-        </div>`,
-        )
-        .join(' ')}
-      </div>`,
-      showCloseButton: true,
-      onBeforeOpen: () => {
-        modalOpen = true;
-      },
-      onAfterClose: () => {
-        modalOpen = false;
-        // Anyline.startScanning(); does not work right now when stopScanning before
-      },
-    });
-  };
-
-  Anyline.onReport = msg => {
-    console.log('Report: ', msg);
-  };
-
-  Anyline.onDebug = msg => {
-    alert(JSON.stringify(msg));
+    alert(JSON.stringify(result.result, null, 2))
   };
 
   Anyline.onError = ({ code, message }) => {
-    if (code === errorCodes.WEBCAM_ERROR) {
+    if (code === window.anylinejs.errorCodes.WEBCAM_ERROR) {
       console.error('webcam error: ', message);
     }
   };
@@ -124,14 +57,25 @@ function mountAnylineJS(preset) {
     console.log('ANYLINE LOADED on main thread');
   };
 
-  Anyline.startScanning();
+  
+  Anyline.startScanning().catch(e => alert(e.message))
 
-  window.Anyline = Anyline;
+}catch(e){
+  alert(e.message);
+  console.error(e)
+}
 }
 
 function remountAnylineJS() {
   Anyline.stopScanning();
   Anyline.dispose();
-  mountAnylineJS(selectedPreset);
+  mountAnylineJS();
 }
 
+function enableFlash() {
+  Anyline.activateFlash(true);
+}
+
+function disableFlash() {
+  Anyline.activateFlash(false);
+}
