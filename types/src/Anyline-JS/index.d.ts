9b384d85c710eb24618377310f043d0b81d62ffb
matze-anyline <matthias.klan@anyline.com>
2021-06-02 18:37:07 +0200

    add types

---
+++ types/src/Anyline-JS/index.d.ts
@@ -0,0 +1,5 @@
+import { errorCodes } from './interface/init.deprecated';
+import { init } from './interface/init';
+import * as Types from './interface/types';
+export { init, errorCodes, // deprecated
+Types, };
