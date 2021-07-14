77d9fce4527d1a634a68d9417a1da5ddff0ff7ee
Katie Ramire <katie@anyline.com>
2021-07-14 18:41:40 +0200

    release 32.0.0

--- demo/index.js
+++ demo/index.js
@@ -1,57 +1,98 @@
 const root = document.getElementById('root');
 let selectedPreset = undefined;
-let Anyline;
+let anyline;
 
-function mountAnylineJS(preset) {
-  try{
+async function mountAnylineJS(preset) {
+  try {
+    selectedPreset = preset;
 
-  selectedPreset = preset;
+    anyline = window.anylinejs.init({
+      config: {},
+      preset: preset.value,
+      license: demoLicense,
+      element: root,
+      debugAnyline: true,
+      anylinePath: '../anylinejs',
+    });
 
-  Anyline = window.anylinejs.init({
-    config: {},
-    preset: preset.value,
-    viewConfig: {},
-    license: demoLicense,
-    element: root,
-    debugAnyline: true,
-    anylinePath: '../anylinejs'
+    anyline.onResult = result => {
+      console.log('Result: ', result);
+      alert(JSON.stringify(result.result, null, 2));
+    };
+
+    await anyline.startScanning().catch(e => alert(e.message));
+
+    appendCameraSwitcher(anyline);
+  } catch (e) {
+    alert(e.message);
+    console.error(e);
+  }
+}
+
+function appendCameraSwitcher(anyline) {
+  if (document.getElementById('cameraSwitcher')) return;
+
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(() => {
+      navigator.mediaDevices.enumerateDevices().then(devices => {
+        renderSelect({
+          options: devices
+            .filter(m => m.kind === 'videoinput')
+            .map(camera => ({
+              text: camera.label,
+              value: camera.deviceId,
+            })),
+          onSelect: deviceId => anyline.setCamera(deviceId),
+        });
+      });
+    });
+}
+
+function renderSelect({ options, onSelect }) {
+  var parent = document.getElementsByClassName('toolbar')[0];
+
+  //Create and append select list
+  const selectEl = document.createElement('select');
+  selectEl.id = 'cameraSwitcher';
+  parent.appendChild(selectEl);
+
+  options.forEach(option => {
+    const optionEl = document.createElement('option');
+    optionEl.value = option.value;
+    optionEl.text = option.text;
+    selectEl.appendChild(optionEl);
   });
 
-  Anyline.onResult = result => {
-    console.log('Result: ', result);
-    alert(JSON.stringify(result.result, null, 2))
-  };
-
-  Anyline.onError = ({ code, message }) => {
-    if (code === window.anylinejs.errorCodes.WEBCAM_ERROR) {
-      console.error('webcam error: ', message);
-    }
-    console.error(code, message)
-  };
-
-  Anyline.onLoad = () => {
-    console.log('ANYLINE LOADED');
-  };
-
-  
-  Anyline.startScanning().catch(e => alert(e.message))
-
-}catch(e){
-  alert(e.message);
-  console.error(e)
-}
+  selectEl.onchange = e => onSelect(e.target.value);
 }
 
 function remountAnylineJS() {
-  Anyline.stopScanning();
-  Anyline.dispose();
+  anyline.stopScanning();
+  anyline.dispose();
   mountAnylineJS(selectedPreset);
 }
 
-function enableFlash() {
-  Anyline.activateFlash(true);
+async function enableFlash() {
+  try {
+    await anyline.activateFlash(true);
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
-function disableFlash() {
-  Anyline.activateFlash(false);
+async function disableFlash() {
+  try {
+    await anyline.activateFlash(false);
+  } catch (e) {
+    alert(e.message);
+  }
+}
+
+async function refocus() {
+  try {
+    await anyline.refocus();
+  } catch (e) {
+    alert(e.message);
+  }
 }
--- types/src/Anyline-JS/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { errorCodes } from './interface/init.deprecated';
-import { init } from './interface/init';
-import * as Types from './interface/types';
-export { init, errorCodes, // deprecated
-Types, };
